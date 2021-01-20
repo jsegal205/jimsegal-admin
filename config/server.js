f16b0367eb6bf9c3aca4ef79cfbb3de46e14be67
Jim Segal <hello@jimsegal.com>
2021-01-19 19:27:43 -0600

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '001e2adbbdf603f99de51970bfe41e18'),
+    },
+  },
+});
