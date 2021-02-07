67d07a891f8f19a343e8449bafeb98484a7695ee
Jim Segal <hello@jimsegal.com>
2021-02-06 18:51:11 -0600

    update slug type and findOne by slug

--- api/recipe/models/recipe.js
+++ api/recipe/models/recipe.js
@@ -1,21 +1,3 @@
 "use strict";
 
-const slugify = require("slugify");
-
-module.exports = {
-  /**
-   * Triggered before user creation.
-   */
-  lifecycles: {
-    async beforeCreate(data) {
-      if (data.title) {
-        data.slug = slugify(data.title, { lower: true });
-      }
-    },
-    async beforeUpdate(params, data) {
-      if (data.title) {
-        data.slug = slugify(data.title, { lower: true });
-      }
-    },
-  },
-};
+module.exports = {};
