759debafcdc0e95f99ee20f8951f25b7d398c8b2
Jim Segal <hello@jimsegal.com>
2021-03-02 22:23:41 -0600

    add custom findOne and find controller actions

--- api/recipe/controllers/recipe.js
+++ api/recipe/controllers/recipe.js
@@ -1,16 +1,18 @@
 const { sanitizeEntity } = require("strapi-utils");
 
+const find = async () => {
+  const entities = await strapi.services.recipe.find({ _sort: "title:asc" });
+  return sanitizeEntity(entities, { model: strapi.models.recipe });
+};
+
+const findOne = async (ctx) => {
+  const { slug } = ctx.params;
+
+  const entity = await strapi.services.recipe.findOne({ slug });
+  return sanitizeEntity(entity, { model: strapi.models.recipe });
+};
+
 module.exports = {
-  /**
-   * Retrieve a record.
-   *
-   * @return {Object}
-   */
-
-  async findOne(ctx) {
-    const { slug } = ctx.params;
-
-    const entity = await strapi.services.recipe.findOne({ slug });
-    return sanitizeEntity(entity, { model: strapi.models.recipe });
-  },
+  find,
+  findOne,
 };
