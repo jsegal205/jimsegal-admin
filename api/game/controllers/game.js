b3316ba6748aec25d8fb121429f81394c1e4de27
Jim Segal <hello@jimsegal.com>
2021-10-19 20:34:50 -0500

    update to fetch query, sort by name

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -2,31 +2,13 @@
 
 const { sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
-
-const columns = ["name", "url", "image_url", "bgg_id"];
-
-const find = async (ctx) => {
-  const result = await strapi.query("game").model.fetchAll({ columns });
-
-  ctx.send(result);
+const find = async () => {
+  const entities = await strapi.services.game.find({
+    _sort: "name:asc",
+  });
+  return sanitizeEntity(entities, { model: strapi.models.game });
 };
-
-// const findOne = async (ctx) => {
-//   const { id } = ctx.params;
-
-//   const result = await strapi
-//     .query("game")
-//     .model.where({ id })
-//     .fetch({ columns });
-
-//   ctx.send(result);
-// };
 
 module.exports = {
   find,
-  // findOne,
 };
