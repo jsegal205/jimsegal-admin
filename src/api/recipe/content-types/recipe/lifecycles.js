9e2af636d17f35334a8347fbf4d681e15c589433
Jim Segal <hello@jimsegal.com>
2022-11-22 15:19:56 -0600

    rm redundant lifecycles key

--- src/api/recipe/content-types/recipe/lifecycles.js
+++ src/api/recipe/content-types/recipe/lifecycles.js
@@ -4,16 +4,14 @@ module.exports = {
   /**
    * Triggered before user creation.
    */
-  lifecycles: {
-    async beforeCreate(data) {
-      if (data.title) {
-        data.slug = slugify(data.title, { lower: true, strict: true });
-      }
-    },
-    async beforeUpdate(params, data) {
-      if (data.title) {
-        data.slug = slugify(data.title, { lower: true, strict: true });
-      }
-    },
+  async beforeCreate(data) {
+    if (data.title) {
+      data.slug = slugify(data.title, { lower: true, strict: true });
+    }
+  },
+  async beforeUpdate(params, data) {
+    if (data.title) {
+      data.slug = slugify(data.title, { lower: true, strict: true });
+    }
   },
 };
