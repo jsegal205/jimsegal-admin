6a3fd722b79bd07519486dc571b234383d33c00b
Jim Segal <hello@jimsegal.com>
2022-11-22 14:30:36 -0600

    migrate API to v4 structure

---
+++ src/api/destination-visit/controllers/destination-visit.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * destination-visit controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::destination-visit.destination-visit');
---
+++ src/api/destination-visit/services/destination-visit.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * destination-visit service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::destination-visit.destination-visit');
